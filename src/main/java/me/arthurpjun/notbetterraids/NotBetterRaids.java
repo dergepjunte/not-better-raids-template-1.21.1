6aaeb6bbf54b2fdbb441f34766a68cfd944f68b0
Malek Bajbouj <malek@MacBook-Pro-6.local>
2025-02-02 18:00:42 +0700

    Initial commit

---
+++ src/main/java/me/arthurpjun/notbetterraids/NotBetterRaids.java
@@ -0,0 +1,16 @@
+package me.arthurpjun.notbetterraids;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NotBetterRaids implements ModInitializer {
+	public static final String MOD_ID = "not-better-raids";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/me/arthurpjun/notbetterraids/NotBetterRaidsClient.java
@@ -0,0 +1,10 @@
+package me.arthurpjun.notbetterraids;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NotBetterRaidsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
